b24eb05f4a57149f8d323eeee5f633198bb4aba2
Laviniiabenvindo <laviniiabenvindo@gmail.com>
2025-02-24 19:56:00 -0300

    Header em andamento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        backgroundColor: {
+          primary: "rgb(64, 94, 245)",
+          secondary: "rgb(46, 229, 242)",
+          linear: "linear-gradient(45deg, rgb(46, 229, 242) 50%, rgb(64, 94, 245) 100%)",
+        }
       },
     },
   },
