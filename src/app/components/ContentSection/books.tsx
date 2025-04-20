bb90af2eb8f58921ce093513c6cdc20e59f0b50a
Laviniiabenvindo <laviniiabenvindo@gmail.com>
2025-04-20 09:06:10 -0300

    Projeto finalizado

--- src/app/components/ContentSection/books.tsx
+++ src/app/components/ContentSection/books.tsx
@@ -26,24 +26,49 @@ export default function ContentSection() {
   setIndex((params) => (params === images.length - 1 ? 0 : params + 1));
  };
  return (
-  <Box className="flex flex-col items-center  my-5 justify-between w-full h-screen">
-   <p className="text-2xl pt-5">eBooks Publicados</p>
-   <div className="flex items-center gap-4">
-    <Button onClick={handlePrev}> <ArrowBack /> </Button>
-    <div className="relative h-48 w-48">
-     <Image
-      src={images[index]}
-      alt={`Look ${index + 1}`}
-      fill
-      className="object-center rounded-lg"
-     />
+  <div>
+   {isLargeScreen && (
+    <Box className="flex flex-col items-center  my-5 justify-between w-full h-screen">
+    <p className="text-2xl pt-5">eBooks Publicados</p>
+    <div className="flex items-center gap-4">
+     <Button onClick={handlePrev}> <ArrowBack /> </Button>
+     <div className="relative h-64 w-64">
+      <Image
+       src={images[index]}
+       alt={`Look ${index + 1}`}
+       fill
+       className="object-center rounded-lg"
+      />
+     </div>
+     <Button onClick={handleNext}> <ArrowForward /> </Button>
     </div>
-    <Button onClick={handleNext}> <ArrowForward /> </Button>
-   </div>
-   <p className="text-justify w-4/5 text-sm">
+    <p className="text-justify w-1/2 text-sm">
+    Este eBook apresenta uma abordagem prática e direta sobre requisitos de software, dividido em três módulos. No primeiro módulo, você aprenderá sobre os tipos de requisitos, como gerenciar mudanças, evitar o aumento de escopo e entender a relação entre requisitos e design. O segundo módulo foca em capturar as necessidades do cliente de forma eficaz, utilizando casos de uso e ferramentas visuais como wireframes e storyboards. Já o terceiro módulo mostra como adaptar os requisitos às metodologias ágeis, utilizando histórias de usuários, backlog e mapas de histórias para organizar e priorizar as funcionalidades. Um guia essencial para quem deseja desenvolver software realmente alinhado às necessidades do cliente.
+    </p>
+    <Button variant="contained" href="https://hotmart.com/pt-br/marketplace/produtos/necessidade-do-cliente-e-requisito-de-software/Y98319589B " color="secondary" target="_blank">Compre agora!</Button>
+   </Box>
+   )}
+   {(isMediumScreen || isSmallScreen) && (
+    <Box className="flex flex-col items-center  my-5 justify-between w-full h-screen">
+    <p className="text-2xl pt-5">eBooks Publicados</p>
+    <div className="flex items-center gap-4">
+     <Button onClick={handlePrev}> <ArrowBack /> </Button>
+     <div className="relative h-48 w-48">
+      <Image
+       src={images[index]}
+       alt={`Look ${index + 1}`}
+       fill
+       className="object-center rounded-lg"
+      />
+     </div>
+     <Button onClick={handleNext}> <ArrowForward /> </Button>
+    </div>
+    <p className="text-justify w-4/5 text-sm">
     Este eBook apresenta uma abordagem prática e direta sobre requisitos de software. Um guia essencial para quem deseja desenvolver software realmente alinhado às necessidades do cliente.
-   </p>
-   <Button variant="contained" href="https://hotmart.com/pt-br/marketplace/produtos/necessidade-do-cliente-e-requisito-de-software/Y98319589B " color="secondary" target="_blank">Compre agora!</Button>
-  </Box>
+    </p>
+    <Button variant="contained" href="https://hotmart.com/pt-br/marketplace/produtos/necessidade-do-cliente-e-requisito-de-software/Y98319589B " color="secondary" target="_blank">Compre agora!</Button>
+   </Box>
+   )}
+  </div>
  )
 }
